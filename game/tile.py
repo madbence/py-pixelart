1bb061a7eb405a15d293678cb7798c56945d91a8
Bence Dányi <bence@danyi.me>
2018-06-03 11:12:17 +0200

    feat: use image size to determine texture size

--- game/tile.py
+++ game/tile.py
@@ -92,7 +92,7 @@ class BaseTileRenderer(TileRenderer):
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE)
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, tile_info.width, tile_info.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, img_data)
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, img.width, img.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, img_data)
 
         super(BaseTileRenderer, self).__init__(shader, vao, tex, screen)
 
